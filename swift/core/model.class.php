88fc7bc44d0004b56a9d2825b60dfc89c9d392ba
ProfoundBlue <profoundblue@163.com>
2016-07-11 16:02:31 +0800

    008++

--- swift/core/model.class.php
+++ swift/core/model.class.php
@@ -4,100 +4,123 @@ declare(strict_types = 1);
 namespace Swift;
 
 class Model {
-	protected $name = '';
-	protected $datas = array();
+	protected $name = null;
+	protected $datas = array ();
 	protected $database = null;
 	
 	/**
 	 * void public function __construct(string $name=null, string $server=null)
 	 */
 	public function __construct(string $name = null, $dsn = null) {
-		if ($this->isCamelCaseRegular( $name )) $this->name = $name;
-		$this->database( $dsn );
+		if ($this->isCamelCaseRegular ( $name )) $this->name = $name;
+		$this->database ( $dsn );
 	}
 	
 	/**
-	 * boolean public function database(string $configKey=null)
+	 * boolean public function database(string $key=null)
 	 */
-	public function database(string $key=null) {
-		$dsns = is_null( $key ) ? C( 'databases_dsn' ) : C( $key );
-		if (! is_array( $datas ) or empty( $datas )) return false;
-		elseif (! $this->isIntSeq( array_keys( $datas ), true )) return false;
-		foreach ( $datas as $data ) {
-			if (! $this->isDsn( $data )) return false;
+	public function database(string $key = null): bool {
+		$dsns = is_null ( $key ) ? C ( 'database_dsn' ) : C ( $key );
+		if (! is_array ( $dsns ) or empty ( $dsns )) return false;
+		elseif (! $this->isIntSeq ( array_keys ( $dsns ), true )) return false;
+		foreach ( $dsns as $dsn ) {
+			if (! $this->isDsn ( $dsn )) return false;
 		}
 		if ($this->database) {
-			$this->database->close();
+			$this->database->close ();
 			$this->database = null;
 		}
-		$this->database = new \Swift\Mysql( $datas );
+		$this->database = new \Swift\Mysql ( $dsns );
 		return $this->database ? true : false;
 	}
 	
 	/**
 	 * Model public function distinct(boolean $data=null)
 	 */
-	public function distinct(bool $data = null) {
+	public function distinct(bool $data = null): Model {
 		if ($this->database) {
-			if (is_bool( $data )) $data = $data ? 'distinct' : 'all';
-			$this->database->data( 'distinct', $data );
+			if (is_null ( $data )) $this->database->clearData ( 'distinct' );
+			else {
+				$data = $data ? 'distinct' : 'all';
+				$this->database->data ( 'distinct', $data );
+			}
 		}
 		return $this;
 	}
 	
 	/**
-	 * Model public function field(string $datas=null)
+	 * Model public function field(string $data=null)
 	 */
-	public function field(string $datas = null) {
-		if ($this->database) $this->database->data( 'field', $datas );
+	public function field(string $data = null): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'field' );
+			else $this->database->data ( 'field', $data );
+		}
 		return $this;
 	}
 	
 	/**
-	 * Model public function table(string $datas=null)
+	 * Model public function table(string $data=null)
 	 */
-	public function table(string $datas = null) {
-		if ($this->database) $this->database->data( 'table', $datas );
+	public function table(string $data = null): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'table' );
+			else $this->database->data ( 'table', $data );
+		}
 		return $this;
 	}
 	
 	/**
 	 * Model public function join(string $data=null)
 	 */
-	public function join(string $data) {
-		if ($this->database) $this->database->data( 'join', $data );
+	public function join(string $data): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'join' );
+			else $this->database->data ( 'join', $data );
+		}
 		return $this;
 	}
 	
 	/**
 	 * Model public function where(string $data=null)
 	 */
-	public function where(string $data = null) {
-		if ($this->database) $this->database->data('where',$data)
+	public function where(string $data = null): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'where' );
+			else $this->database->data ( 'where', $data );
+		}
 		return $this;
 	}
 	
 	/**
-	 * Model public function group(string $datas=null)
+	 * Model public function group(string $data=null)
 	 */
-	public function group(string $data) {
-		if ($this->database) $this->database->data( 'group', $data );
+	public function group(string $data=null): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'group' );
+			else $this->database->data ( 'group', $data );
+		}
 		return $this;
 	}
 	
 	/**
 	 * Model public function having(string $data=null)
 	 */
-	public function having(string $data) {
-		if ($this->database) $this->database->data( 'having', $data );
+	public function having(string $data=null): Model {
+		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'having' );
+			else $this->database->data ( 'having', $data );
+		}
 		return $this;
 	}
 	
 	/**
-	 * Model public function order(string $datas=null)
+	 * Model public function order(string $data=null)
 	 */
 	public function order($datas) {
 		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'order' );
+			else $this->database->data ( 'order', $data );
 		}
 		return $this;
 	}
@@ -107,6 +130,8 @@ class Model {
 	 */
 	public function limit(string $data = null) {
 		if ($this->database) {
+			if (is_null ( $data )) $this->database->clearData ( 'limit' );
+			else $this->database->data ( 'limit', $data );
 		}
 		return $this;
 	}
@@ -115,29 +140,29 @@ class Model {
 	 * array public function create([array $fields=array(string $field,...)])
 	 */
 	public function create($fields = array()) {
-		$this->clear();
-		if (! is_array( $fields )) return $this->datas;
-		elseif (! empty( $fields )) {
-			if (! $this->isIntSeq( array_keys( $fields ), true )) return $this->datas;
+		$this->clear ();
+		if (! is_array ( $fields )) return $this->datas;
+		elseif (! empty ( $fields )) {
+			if (! $this->isIntSeq ( array_keys ( $fields ), true )) return $this->datas;
 			foreach ( $fields as &$value ) {
-				if (! $this->isCamelCaseRegular( $value )) return $this->datas;
-				$value = $this->camelCaseToDbRegular( $value );
+				if (! $this->isCamelCaseRegular ( $value )) return $this->datas;
+				$value = $this->camelCaseToDbRegular ( $value );
 			}
 		}
-		if (! empty( $_POST )) {
+		if (! empty ( $_POST )) {
 			$datas = $_POST;
 			foreach ( $datas as $key => &$value ) {
-				if (! empty( $fields ) && ! in_array( $key, $fields, true )) unset( $value );
-				elseif (! $this->isDbRegular( $key )) unset( $value );
-				elseif (is_array( $value )) $value = implode( '{}', $value );
+				if (! empty ( $fields ) && ! in_array ( $key, $fields, true )) unset ( $value );
+				elseif (! $this->isDbRegular ( $key )) unset ( $value );
+				elseif (is_array ( $value )) $value = implode ( '{}', $value );
 			}
-			if (! empty( $datas ) && $this->name && $this->database) {
-				$fields = $this->database->fields( $this->name );
+			if (! empty ( $datas ) && $this->name && $this->database) {
+				$fields = $this->database->fields ( $this->name );
 				if ($fields) {
-					$names = array_keys( $fields );
+					$names = array_keys ( $fields );
 					foreach ( $datas as $key => &$value ) {
-						if (! in_array( $key, $names, true )) unset( $value );
-						else $value = $this->changeDataType( $value, $this->mapDataType( $fields [$key] ['type'] ) );
+						if (! in_array ( $key, $names, true )) unset ( $value );
+						else $value = $this->changeDataType ( $value, $this->mapDataType ( $fields [$key] ['type'] ) );
 					}
 				}
 			}
@@ -150,18 +175,18 @@ class Model {
 	 * array public function data(array $datas=array(string $field=>scalar $value,...))
 	 */
 	public function data($datas) {
-		$this->clear();
-		if (is_array( $datas ) && ! empty( $datas )) {
+		$this->clear ();
+		if (is_array ( $datas ) && ! empty ( $datas )) {
 			foreach ( $datas as $key => &$value ) {
-				if (! $this->isCamelCaseRegular( $key )) unset( $value );
-				elseif (! is_scalar( $value ) && ! is_null( $value )) unset( $value );
+				if (! $this->isCamelCaseRegular ( $key )) unset ( $value );
+				elseif (! is_scalar ( $value ) && ! is_null ( $value )) unset ( $value );
 			}
-			if (! empty( $datas ) && $this->name && $this->database) {
-				$fields = $this->database->fields( $this->name );
+			if (! empty ( $datas ) && $this->name && $this->database) {
+				$fields = $this->database->fields ( $this->name );
 				if ($fields) {
-					$names = array_keys( $fields );
+					$names = array_keys ( $fields );
 					foreach ( $datas as $key => &$value ) {
-						if (! in_array( $key, $names, true )) unset( $value );
+						if (! in_array ( $key, $names, true )) unset ( $value );
 					}
 				}
 			}
@@ -176,110 +201,80 @@ class Model {
 	}
 	
 	/**
-	 * void public function clear()
+	 * void public function clear(void)
 	 */
 	public function clear() {
-		$this->datas = array();
+		$this->datas = array ();
 	}
 	
 	/**
 	 * array public function select(void)
 	 */
-	public function select() {
-		return $this->database ? $this->database->select() : array();
+	public function select(): array {
+		return $this->database ? $this->database->select () : array ();
 	}
 	
 	/**
 	 * integer public function add(void)
 	 */
-	public function add() {
-		return $this->database ? $this->database->insert( $this->datas ) : - 1;
+	public function add(): int {
+		return $this->database ? $this->database->insert ( $this->datas ) : - 1;
 	}
 	
 	/**
-	 * boolean|integer public function save(void)
+	 * integer public function save(void)
 	 */
-	public function save() {
-		return $this->database ? $this->database->update( $this->datas ) : false;
+	public function save(): int {
+		return $this->database ? $this->database->update ( $this->datas ) : 0;
 	}
 	
 	/**
-	 * boolean|integer public function delete(void)
+	 * integer public function delete(void)
 	 */
 	public function delete() {
-		return $this->database ? $this->database->delete() : false;
+		return $this->database ? $this->database->delete () : 0;
 	}
 	
 	/**
 	 * boolean protected function isDbRegular(string $data)
 	 */
-	protected function isDbRegular($data) {
-		if (is_string( $data ) && $data != '') {
-			$pattern = '/^([a-z]+_)*[a-z]+$/';
-			return preg_match( $pattern, $data ) ? true : false;
-		}
-		return false;
+	protected function isDbRegular(string $data): bool {
+		$pattern = '/^([a-z]+_)*[a-z]+$/';
+		return preg_match ( $pattern, $data ) ? true : false;
 	}
 	
 	/**
 	 * boolean protected function isDbRegularPlus(string $data)
 	 */
-	protected function isDbRegularPlus($data) {
-		if (is_string( $data ) && $data != '') {
-			$arr = explode( '.', $data );
-			if (count( $arr ) != 2) return false;
-			foreach ( $arr as $value ) {
-				if (! $this->isDbRegular( $value )) return false;
-			}
-			return true;
+	protected function isDbRegularPlus(string $data): bool {
+		$dataSections = explode ( '.', $data );
+		if (count ( $dataSections ) != 2) return false;
+		foreach ( $dataSections as $value ) {
+			if (! $this->isDbRegular ( $value )) return false;
 		}
-		return false;
+		return true;
 	}
 	
 	/**
 	 * boolean protected function isCamelCaseRegular(string $data)
 	 */
 	protected function isCamelCaseRegular($data) {
-		if (is_string( $data )) {
+		if (is_string ( $data )) {
 			$pattern = '/^[a-z]+([A-Z][a-z]*)*$/';
-			return preg_match( $pattern, $data ) ? true : false;
+			return preg_match ( $pattern, $data ) ? true : false;
 		}
 		return false;
-	}
-	
-	/**
-	 * boolean protected function isSingleDsn(array $datas=array(string $key=>string|integer $value,...))
-	 */
-	protected function isSingleDsn($datas) {
-		return $this->isDsn( $datas );
-	}
-	
-	/**
-	 * boolean protected function isDistributedDsn(array $datas=array('reads'=>array(...),'writes'=>array(...)))
-	 */
-	protected function isDistributedDsn($datas) {
-		if (! is_array( $datas ) or empty( $datas )) return false;
-		foreach ( $datas as $index => $data ) {
-			if (! in_array( $index, array( 'reads', 'writes' ), true )) return false;
-			elseif (! is_array( $data ) or empty( $data )) return false;
-			elseif (! $this->isIntSeq( array_keys( $data ), true )) return false;
-			foreach ( $data as $value ) {
-				if (! $this->isDsn( $value )) return false;
-			}
-		}
-		return true;
 	}
 	
 	/**
 	 * boolean protected function isDsn(array $datas=array(string $key=>string|integer $value,...))
 	 */
 	protected function isDsn($datas) {
-		
-		if (is_array( $datas ) && ! empty( $datas )) {
+		if (is_array ( $datas ) && ! empty ( $datas )) {
 			foreach ( $datas as $key => $value ) {
-				if (! in_array( $key, array( 'type', 'host', 'port', 'user', 'pwd', 'database', 'charset' ), true )) return false;
-				elseif ('port' == $value && ! is_integer( $value )) return false;
-				elseif (! is_string( $value ) or '' == $value) return false;
+				if (! in_array ( $key, array ('type','host','port','user','pwd','database','charset' ), true )) return false;
+				elseif ('port' == $value && ! is_integer ( $value )) return false;
+				elseif (! is_string ( $value ) or '' == $value) return false;
 			}
 			return true;
 		}
@@ -290,10 +285,10 @@ class Model {
 	 * boolean|string protected function camelCaseToDbRegular(string $data)
 	 */
 	protected function camelCaseToDbRegular($data) {
-		if (is_string( $prop ) && $this->isCamelCaseRegular( $data )) {
+		if (is_string ( $prop ) && $this->isCamelCaseRegular ( $data )) {
 			$pattern = '/([A-Z])/';
 			$replace = '_$1';
-			return strtolower( preg_replace( $pattern, $replace, $data ) );
+			return strtolower ( preg_replace ( $pattern, $replace, $data ) );
 		}
 		return false;
 	}
@@ -302,50 +297,24 @@ class Model {
 	 * boolean protected function rule(array $datas=array(array(str $field, str $rule[=>mixed $value], str $errMessage),...)
 	 */
 	protected function rule($datas) {
-		if (is_array( $datas ) && ! empty( $datas )) {
-			if (! $this->isSequence( $datas )) return false;
-			foreach ( $datas as $index => $data ) {
-				if (! is_integer( $index )) return false;
-				elseif (! $this->isRule( $data )) return false;
-			}
-			$this->rules = $datas;
-			return true;
-		}
-		return false;
+		
 	}
 	
 	/**
-	 * boolean protected function isRule(array $datas=array(str $field, str $rule[=>mixed $value], str $errMessage))
+	 * boolean protected function isRuleRegular(array $datas=array(str $field, str $rule[=>mixed $value], str $errMessage))
 	 */
-	protected function isRule($datas) {
-		if (is_array( $datas ) && ! empty( $datas )) {
-			if (! $this->isSequence( $datas )) return false;
-			switch (count( $datas )) {
-				case 3 :
-					list ( $key1, $key2, $key3 ) = array_keys( $datas );
-					list ( $field, $value2, $errMessage ) = array_values( $datas );
-					break;
-				default :
-					return false;
-					break;
-			}
-			if (! is_integer( $key1 )) return false;
-			elseif (! $this->isCamelCaseRegular( $field )) return false;
-			elseif (! $this->integer( $key3 )) return false;
-			elseif (! is_string( $errMessage )) return false;
-			return true;
-		}
-		return false;
+	protected function isRuleRegular(array $datas): bool {
+		
 	}
 	
 	/**
 	 * boolean|string protected function mapDataType(string $type)
 	 */
 	protected function mapDataType($type) {
-		if ($this->database && is_string( $type ) && $type != '') {
-			$maps = $this->database->map();
+		if ($this->database && is_string ( $type ) && $type != '') {
+			$maps = $this->database->map ();
 			foreach ( $maps as $index => $map ) {
-				if (in_array( $type, $map, true )) return $index;
+				if (in_array ( $type, $map, true )) return $index;
 			}
 		}
 		return false;
@@ -355,8 +324,8 @@ class Model {
 	 * mixed protected function changeDataType(scalar $data, string $type)
 	 */
 	protected function changeDataType($data, $type) {
-		if (! is_scalar( $data ) && ! is_null( $data )) return $data;
-		elseif (! is_string( $type ) or '' == $type) return $data;
+		if (! is_scalar ( $data ) && ! is_null ( $data )) return $data;
+		elseif (! is_string ( $type ) or '' == $type) return $data;
 		switch ($type) {
 			case 'string' :
 				return ( string ) $data;
@@ -383,9 +352,9 @@ class Model {
 	 * boolean protected function isIntSeq(array $datas [,boolean $mode])
 	 */
 	protected function isIntSeq($datas, $mode = false) {
-		if (! is_array( $datas ) or empty( $data )) return false;
-		elseif (! is_bool( $mode )) return false;
-		$values = $mode ? array_filter( array_values( $datas ), 'is_integer' ) : array_values( $datas );
+		if (! is_array ( $datas ) or empty ( $data )) return false;
+		elseif (! is_bool ( $mode )) return false;
+		$values = $mode ? array_filter ( array_values ( $datas ), 'is_integer' ) : array_values ( $datas );
 		foreach ( $values as $key => $value ) {
 			if ($key != $value) return false;
 		}
@@ -396,11 +365,11 @@ class Model {
 	 * boolean protected function isStrSeq(array $datas [,boolean $mode])
 	 */
 	protected function isStrSeq($datas, $mode = false) {
-		if (! is_array( $datas ) or empty( $data )) return false;
-		elseif (! is_bool( $mode )) return false;
-		$values = array_values( $datas );
+		if (! is_array ( $datas ) or empty ( $data )) return false;
+		elseif (! is_bool ( $mode )) return false;
+		$values = array_values ( $datas );
 		foreach ( $values as $value ) {
-			if (! is_string( $value )) return false;
+			if (! is_string ( $value )) return false;
 			elseif ($mode && '' == $value) return false;
 		}
 		return true;
